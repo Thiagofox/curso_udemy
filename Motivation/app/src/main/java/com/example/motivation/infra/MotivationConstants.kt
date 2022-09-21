0d624ec708ac763685732b3b49add66959f29549
Thiago Cavalcante <thiagofox00@gmail.com>
2022-09-21 17:09:30 -0300

    aplicação Motivation finalizada aula 143

--- Motivation/app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
+++ Motivation/app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
@@ -1,8 +1,16 @@
 package com.example.motivation.infra
 
 class MotivationConstants private constructor() {
+    // ninguem deve instanciar a classe motivation constants portanto ele tem o construtor fechado
+    // [private constructor]
     object KEY {
         const val  USER_NAME = "USER_NAME"
+    }
+
+    object FILTER {
+        const val ALL = 1
+        const val HAPPY = 2
+        const val SUNNY = 3
     }
 
 }
--- Motivation/app/src/main/java/com/example/motivation/ui/MainActivity.kt
+++ Motivation/app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.motivation.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.motivation.infra.MotivationConstants
 import com.example.motivation.R
+import com.example.motivation.data.Mock
 import com.example.motivation.infra.SecurityPreferences
 import com.example.motivation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var categoryId = MotivationConstants.FILTER.ALL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +28,61 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         // esconde a barra inicial com o nome do app
 
         binding.buttonNewPhrase.setOnClickListener(this)
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
+
         // evento de click
 
         handleUserName()
+        handleFilter(R.id.image_all)
+        handleNextPhrase()
+    }
+
+    private fun handleNextPhrase() {
+        val phrase = Mock().getPhrase(categoryId)
+        binding.textMessage.text = phrase
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phrase){
-            Toast.makeText(this, "FUI CLICADO", Toast.LENGTH_LONG).show()
+           handleNextPhrase()
+        } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)){
+            handleFilter(view.id)
+
         }
     }
 
     private fun handleUserName() {
         val name = SecurityPreferences(this).getString(MotivationConstants.KEY.USER_NAME)
         binding.textUserName.text = "Olá, $name!"
+    }
+    private fun handleFilter(id: Int) {
+
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+
+        when (id) {
+            R.id.image_all -> {
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.ALL
+            }
+            R.id.image_happy -> {
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.HAPPY
+            }
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.SUNNY
+            }
+        }
+//        if (id == R.id.image_all){
+//            binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+//        } else if (id == R.id.image_happy) {
+//            binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+//        } else if(id == R.id.image_sunny) {
+//            binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+//        }
     }
 }
